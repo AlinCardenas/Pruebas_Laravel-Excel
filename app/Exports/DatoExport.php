5fe37340f87474514274f3b4bbf5155fb8881eb0
Alin Cardenas <al222110745@gmail.com>
2023-06-06 10:11:37 -0600

    Antes de encapsulacion

--- app/Exports/DatoExport.php
+++ app/Exports/DatoExport.php
@@ -7,14 +7,12 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
-use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use PhpOffice\PhpSpreadsheet\Style\Fill;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
 use PhpOffice\PhpSpreadsheet\Style\Border;
-
-
-
-
+use PhpOffice\PhpSpreadsheet\Calculation\Calculation;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 class DatoExport implements FromCollection, WithHeadings,WithStyles
 
@@ -23,17 +21,15 @@ class DatoExport implements FromCollection, WithHeadings,WithStyles
     * @return \Illuminate\Support\Collection
     */
     public function styles(Worksheet $sheet)
-    {  
-
-        
-        /* Configuracion de barra arriba del encabezado */
+    { 
+        // Configuración de barra arriba del encabezado
         $sheet->mergeCells('A1:V1');
         $sheet->getRowDimension(1)->setRowHeight(60);
         $sheet->getStyle('A1:V1')->applyFromArray([
             'fill' => [
                 'fillType' => Fill::FILL_SOLID,
                 'startColor' => [
-                    'rgb' => '538DD5', 
+                    'rgb' => '538DD5',
                 ],
             ],
             'font' => [
@@ -46,24 +42,34 @@ class DatoExport implements FromCollection, WithHeadings,WithStyles
             'alignment' => [
                 'wrapText' => true,
                 'vertical' => Alignment::VERTICAL_CENTER,
-                'horizontal' =>Alignment::HORIZONTAL_CENTER,
+                'horizontal' => Alignment::HORIZONTAL_CENTER,
             ],
-            
         ]);
-
-      /*   $styleArray = [
-            'borders' => [
-                'outline' => [
-                    'borderStyle' => Border::BORDER_THIN,
-                    'color' => ['rgb' => '0000FF'], // Código de color azul en formato RGB
-                ],
-            ],
+        
+        /* Tamaño de las celdas en los encabezados */
+        $columnWidths = [
+            'G' => 15,
+            'H' => 15,
+            'I' => 15,
+            'J' => 15,
+            'O' => 15,
+            'P' => 15,
+            'S' => 15,
+            'T' => 15,
+            'U' => 15,
+            'V' => 15,
+            'L' => 25,
+            'K' => 25,
+            'M' => 25,
+            'R' => 25,
+            'B' => 35,
+            'F' => 35,
         ];
-        
-        $sheet->getStyle('A1:V1')->applyFromArray($styleArray); */
-        
-        
-        /* Color de los encabezados */
+        foreach ($columnWidths as $column => $width) {
+            $sheet->getColumnDimension($column)->setWidth($width);
+        }
+
+        // Color de los encabezados
         $sheet->getRowDimension(2)->setRowHeight(41);
         $styleArray = [
             'fill' => [
@@ -78,41 +84,15 @@ class DatoExport implements FromCollection, WithHeadings,WithStyles
                     'rgb' => 'FFFFFF',
                 ],
                 'size' => 11,
-
             ],
             'alignment' => [
                 'wrapText' => true,
                 'vertical' => Alignment::VERTICAL_CENTER,
-                'horizontal' =>Alignment::HORIZONTAL_CENTER,
+                'horizontal' => Alignment::HORIZONTAL_CENTER,
             ],
         ];
 
-        /* Tamaño de las celdas en los encabezados */
-        $columns = ['G','H','I','J','O','P','S','T','U','v'];
-        $width = 15;
-        foreach ($columns as $column) {
-            $sheet->getColumnDimension($column)->setWidth($width);
-        } 
-        $columns = ['L','K','M','R'];
-        $width = 25;
-        foreach ($columns as $column) {
-            $sheet->getColumnDimension($column)->setWidth($width);
-        } 
-        $columns = ['B','F'];
-        $width = 35;
-        foreach ($columns as $column) {
-            $sheet->getColumnDimension($column)->setWidth($width);
-        } 
-        /* Celdas mas pequeñas */
-       /*  $columns = ['G','K', 'L', 'M', 'O', 'P', 'Q', 'S', 'T', 'U', 'V','B','R', 'H', 'J', 'I','F'];
-        $width = 30;
-        foreach ($columns as $column) {
-            $sheet->getColumnDimension($column)->setWidth($width);
-        }
-
- */
-
-        /* Color de las columnas de abajo */
+        /* Color de los encabezados */
         $sheet->getStyle('A2:V2')->applyFromArray($styleArray);
         $sheet->getStyle('G2:I2')->applyFromArray(array_merge($styleArray, [
             'fill' => [
@@ -138,15 +118,19 @@ class DatoExport implements FromCollection, WithHeadings,WithStyles
                 ],
             ],
         ]);
-        $sheet->getStyle('O2:V2')->applyFromArray($styleArray);
 
-        /* Sacarar el numero de celdas */
+        /* Sacarar el numero de filas */
         $highestRow = $sheet->getHighestRow();
+        /* Sacar letra de ultima columan */
         $columnCount = $sheet->getHighestColumn();
 
 
         /* Agregar una columna abajo */
-        $rangeColors = 'A' . ($highestRow + 1) . ':' . $columnCount . ($highestRow + 1);
+        $rangeColors = 'A' . ($highestRow + 1) . ':F' . ($highestRow + 1);
+        $rangeColors2 = 'G' . ($highestRow + 1) . ':I' . ($highestRow + 1);
+        $rangeColors3 = 'J' . ($highestRow + 1) . ':J' . ($highestRow + 1);
+        $rangeColors4 = 'K' . ($highestRow + 1) . ':N' . ($highestRow + 1);
+        $rangeColors5 = 'O' . ($highestRow + 1) . ':V' . ($highestRow + 1);
 
         // Agregar una nueva fila al final de la tabla con colores
         $sheet->insertNewRowBefore($highestRow + 1);
@@ -158,6 +142,50 @@ class DatoExport implements FromCollection, WithHeadings,WithStyles
                 ],
             ],
         ]);
+        $sheet->getStyle($rangeColors2)->applyFromArray([
+            'fill' => [
+                'fillType' => Fill::FILL_SOLID,
+                'startColor' => [
+                    'rgb' => '76933C',
+                ],
+            ],
+        ]);
+        $sheet->getStyle($rangeColors3)->applyFromArray([
+            'fill' => [
+                'fillType' => Fill::FILL_SOLID,
+                'startColor' => [
+                    'rgb' => '963634',
+                ],
+            ],
+        ]);
+        $sheet->getStyle($rangeColors4)->applyFromArray([
+            'fill' => [
+                'fillType' => Fill::FILL_SOLID,
+                'startColor' => [
+                    'rgb' => 'E26B0A',
+                ],
+            ],
+        ]);
+        $sheet->getStyle($rangeColors5)->applyFromArray([
+            'fill' => [
+                'fillType' => Fill::FILL_SOLID,
+                'startColor' => [
+                    'rgb' => '366092',
+                ],
+            ],
+        ]);
+
+        /* $highestColumn = $sheet->getHighestColumn();
+        $sumRange = 'H3:' . $highestColumn . '3';
+
+        $sumFormula = '=SUM(' . $sumRange . ')';
+        $sheet->setCellValue('H' . ($highestRow + 1), $sumFormula); */
+
+        
+        // Agrega la fórmula de suma a la celda A4
+
+
+
 
         /* Asignar colores a las celdas normales */
         $rangeColors = [
@@ -167,7 +195,6 @@ class DatoExport implements FromCollection, WithHeadings,WithStyles
             'K3:N' => 'FDE9D9',
             'O3:V' => 'DCE6F1',
         ];
-       
 
         foreach ($rangeColors as $range => $color) {
             $range .= $highestRow;
@@ -180,45 +207,18 @@ class DatoExport implements FromCollection, WithHeadings,WithStyles
             ],
         ]);
 
-        $rangeBorderColors = [
-            'A3:F' => '95B3D7',
-            'G3:I' => 'C4D79B',
-            'J3:J' => 'DA9694',
-            'K3:N' => 'FABF8F',
-            'O3:V' => '95B3D7',
-        ];
 
-        /* $styleArray = [
-            'borders' => [
-                'bottom' => ['borderStyle' => Border::BORDER_THIN,'color' => ['rgb' => $color]],
-            ],
-        ];
-
-        $sheet->getStyle('A3:F3')->applyFromArray($styleArray); */
-
+        /* $rangeBorderColors = [
+            'A3:F3' => '95B3D7',
+            'G3:I3' => 'C4D79B',
+            'K3:N3' => 'DA9694',
+            'O3:V3' => '95B3D7',
+        ]; */
         
-        foreach ($rangeBorderColors as $range => $color) {
-            $range .= $highestRow;
-            $sheet->getStyle($range)->applyFromArray([
-                'borders' => [
-                    'bottom' => [
-                        'borderStyle' => Border::BORDER_THICK,
-                    ]
-                ],
-            ]);
-        }
-    }
-/* 
-    'borders' => [
-        'top' => [
-            'borderStyle' => Border::BORDER_THIN,
-            'color' => [
-                'rgb' => $color,
-            ],
-        ],
-    ], */
+        
 
     }
+}
     public function headings(): array
     {
         return [
